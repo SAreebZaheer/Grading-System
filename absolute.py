25a93b7f37beb0e247109538aea34dffd765bc52
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2024-12-14 10:40:42 +0500

    added abslolute.py

---
+++ absolute.py
@@ -0,0 +1,15 @@
+#Absolute Grading
+
+import pandas as pd
+
+def absolute(data):
+    choice = input("Do you want to set your own thresholds or use HEC's predefined threshold?")
+    print("Please set the grade thresholds")
+    A = int(input("Minimum Percentage for A Grade: "))
+    B = int(input("Minimum Percentage for B Grade: "))
+    C = int(input("Minimum Percentage for C Grade: "))
+    D = int(input("Minimum Percentage for D Grade: "))
+    B = int(input("Minimum Percentage for B Grade: "))
+    B = int(input("Minimum Percentage for B Grade: "))
+
+
